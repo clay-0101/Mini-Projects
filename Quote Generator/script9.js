acc6d1990c8d8e83ab5ea1587356c69c9f0a0ee4
clay-0101 <karyon.crypten.dev0111@gmail.com>
2025-09-03 18:50:54 +0530

    Add My 6.Project: Quote Generator

---
+++ Quote Generator/script9.js
@@ -0,0 +1,21 @@
+let quote = document.querySelector(".quote");
+let category = document.getElementById("category");
+let author = document.querySelector(".author");
+let getButton = document.querySelector(".get");
+
+async function getQuote() {
+    const res = await fetch("https://api.api-ninjas.com/v1/quotes", {
+      method: "GET",
+      headers: {
+        "X-Api-Key": "vxA7+RMrhwevasKHJmQKtg==qvcHDvYN6GcBwQ5H"
+      }
+    });
+    const data = await res.json();
+    console.log(data[0].author);
+    quote.innerHTML= data[0].quote;
+    category.innerHTML=`"${data[0].category}"`;
+    author.innerHTML=data[0].author;
+}
+getButton.addEventListener("click",()=>{
+  getQuote();
+})
